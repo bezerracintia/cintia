874487a80fcde7628f7f73e20c001af461afd8a6
bezerracintia <miguelzinfx90@gmail.com>
2025-08-18 20:58:35 -0300

    Add files via upload

---
+++ prog_aula_01_01.py
@@ -0,0 +1,5 @@
+# Programa que calcula o dobro de um número
+print('Programa que calcula o dobro de um número')
+num = int(input('Digite um número inteiro: '))
+dobro = num * 2
+print('O Resultado é:',dobro)
---
+++ prog_aula_01_02 (1).py
@@ -0,0 +1,6 @@
+# Programa que calcula a soma entre dois números inteiros
+print('Programa que calcula a soma entre dois números inteiros')
+num1 = int(input('Digite o primeiro número inteiro: '))
+num2 = int(input('Digite o segundo número inteiro: '))
+soma = num1 + num2
+print('O Resultado da Soma é:',soma)
